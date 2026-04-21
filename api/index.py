35696a4f05bbb45c8a7a9988248f610d6673a872
Manoharan <vinoth.manoharan@syneoshealth.com>
2026-04-21 12:50:59 +0200

    Switch to Vercel native ASGI: export app directly, drop Mangum

--- api/index.py
+++ api/index.py
@@ -1,21 +1,6 @@
 import sys
 import os
-import traceback
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'backend'))
 
-try:
-    from main import app
-    from mangum import Mangum
-    handler = Mangum(app, lifespan="off")
-except Exception as _e:
-    from fastapi import FastAPI
-    from mangum import Mangum as _Mangum
-    _err = traceback.format_exc()
-    _debug = FastAPI()
-
-    @_debug.get("/api/debug")
-    def _debug_error():
-        return {"import_error": str(_e), "traceback": _err}
-
-    handler = _Mangum(_debug)
+from main import app
